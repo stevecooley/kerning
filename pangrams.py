d6a13ef8782bfe4d2dd0b857c8776437e64f6e8a
stevecooley <steve@sc-fa.com>
2023-05-09 21:48:53 -0700

    Add files via upload
    initial commit. Almost all of this code was written by chatGPT, or, suggested anyway.  I did have to piece it together and then I did find chatGPT made a *lot* of contradictory suggestions, so I endured that and curated until it worked.

---
+++ pangrams.py
@@ -0,0 +1,120 @@
+pangrams = """
+Using all UPPERCASE:<br>
+JIVED FOX NYMPH GRABS QUICK WALTZ.
+SPHINX OF BLACK QUARTZ, JUDGE MY VOW.
+THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+HOW VEXINGLY QUICK DAFT ZEBRAS JUMP!
+PACK MY BOX WITH FIVE DOZEN LIQUOR JUGS.
+WALTZ, NYMPH, FOR QUICK JIGS VEX BUD.
+CRAZY FREDERICKA BOUGHT MANY VERY EXQUISITE OPAL JEWELS.
+FIVE QUACKING ZEPHYRS JOLT MY WAX BED.
+THE FIVE BOXING WIZARDS JUMP QUICKLY.
+MY EX PUB QUIZ CROWD GAVE JOYFUL THANKS.
+TWO DRIVEN JOCKS HELP FAX MY BIG QUIZ.
+JACKDAWS LOVE MY BIG SPHINX OF QUARTZ.
+HOW RAZORBACK JUMPING FROGS CAN LEVEL SIX PIQUED GYMNASTS.
+BIG JULY EARTHQUAKES CONFOUND ZANY EXPERIMENTAL VOW.
+THE WIZARD QUICKLY JINXED THE GNOMES BEFORE THEY VAPORIZED.
+WE QUICKLY SEIZED THE BLACK AXLE AND JUST SAVED IT FROM HARM.
+A QUIVERING TEXAS ZOMBIE FOUGHT REPUBLIC LINKED JEWELRY.
+THE EXPLORER WAS FROZEN IN HIS BIG KAYAK JUST AFTER MAKING QUEER DISCOVERIES.
+FEW BLACK TAXIS DRIVE UP MAJOR ROADS ON QUIET HAZY NIGHTS.
+SIXTY ZIPPERS WERE QUICKLY PICKED FROM THE WOVEN JUTE BAG.
+THE QUICK BROWN FOX JUMPS OVER A LAZY PARK RANGER DOG.
+A MAD BOXER SHOT A QUICK, GLOVED JAB TO THE JAW OF HIS DIZZY OPPONENT.
+HEAVY BOXES PERFORM QUICK WALTZES AND JIGS.
+FIVE OR SIX BIG JET PLANES ZOOMED QUICKLY BY THE TOWER.
+A LARGE FAWN JUMPED QUICKLY OVER WHITE ZINC BOXES.<br>
+<br>
+Using all lowercase:
+jived fox nymph grabs quick waltz.
+sphinx of black quartz, judge my vow.
+the quick brown fox jumps over the lazy dog.
+how vexingly quick daft zebras jump!
+pack my box with five dozen liquor jugs.
+waltz, nymph, for quick jigs vex bud.
+crazy fredericka bought many very exquisite opal jewels.
+five quacking zephyrs jolt my wax bed.
+the five boxing wizards jump quickly.
+my ex pub quiz crowd gave joyful thanks.
+two driven jocks help fax my big quiz.
+jackdaws love my big sphinx of quartz.
+how razorback jumping frogs can level six piqued gymnasts.
+big july earthquakes confound zany experimental vow.
+the wizard quickly jinxed the gnomes before they vaporized.
+we quickly seized the black axle and just saved it from harm.
+a quivering texas zombie fought republic linked jewelry.
+the explorer was frozen in his big kayak just after making queer discoveries.
+few black taxis drive up major roads on quiet hazy nights.
+sixty zippers were quickly picked from the woven jute bag.
+the quick brown fox jumps over a lazy park ranger dog.
+a mad boxer shot a quick, gloved jab to the jaw of his dizzy opponent.
+heavy boxes perform quick waltzes and jigs.
+five or six big jet planes zoomed quickly by the tower.
+a large fawn jumped quickly over white zinc boxes.<br>
+<br>
+Uppercased Words:<br>
+Jived Fox Nymph Grabs Quick Waltz.
+Sphinx Of Black Quartz, Judge My Vow.
+The Quick Brown Fox Jumps Over The Lazy Dog.
+How Vexingly Quick Daft Zebras Jump!
+Pack My Box With Five Dozen Liquor Jugs.
+Waltz, Nymph, For Quick Jigs Vex Bud.
+Crazy Fredericka Bought Many Very Exquisite Opal Jewels.
+Five Quacking Zephyrs Jolt My Wax Bed.
+The Five Boxing Wizards Jump Quickly.
+My Ex Pub Quiz Crowd Gave Joyful Thanks.
+Two Driven Jocks Help Fax My Big Quiz.
+Jackdaws Love My Big Sphinx Of Quartz.
+How Razorback Jumping Frogs Can Level Six Piqued Gymnasts.
+Big July Earthquakes Confound Zany Experimental Vow.
+The Wizard Quickly Jinxed The Gnomes Before They Vaporized.
+We Quickly Seized The Black Axle And Just Saved It From Harm.
+A Quivering Texas Zombie Fought Republic Linked Jewelry.
+The Explorer Was Frozen In His Big Kayak Just After Making Queer Discoveries.
+Few Black Taxis Drive Up Major Roads On Quiet Hazy Nights.
+Sixty Zippers Were Quickly Picked From The Woven Jute Bag.
+The Quick Brown Fox Jumps Over A Lazy Park Ranger Dog.
+A Mad Boxer Shot A Quick, Gloved Jab To The Jaw Of His Dizzy Opponent.
+Heavy Boxes Perform Quick Waltzes And Jigs.
+Five Or Six Big Jet Planes Zoomed Quickly By The Tower.
+A Large Fawn Jumped Quickly Over White Zinc Boxes.<br>
+<br>
+tHiS kInD oF cAsInG:<br>
+jIvEd FoX nYmPh GrAbS qUiCk WaLtZ.
+sPhInX oF bLaCk QuArTz, jUdGe My VoW.
+tHe QuIcK bRoWn FoX jUmPs OvEr ThE lAzY dOg.
+hOw VeXiNgLy QuIcK dAfT zEbRaS jUmP!
+pAcK mY bOx WiTh FiVe DoZeN lIqUoR jUgS.
+wAlTz, nYmPh, fOr QuIcK jIgS vEx BuD.
+cRaZy FrEdErIcKa BoUgHt MaNy VeRy ExQuIsItE oPaL jEwElS.
+fIvE qUaCkInG zEpHyRs JoLt My WaX bEd.
+tHe FiVe BoXiNg WiZaRdS jUmP qUiCkLy.
+mY eX pUb QuIz CrOwD gAvE jOyFuL tHaNkS.
+tWo DrIvEn JoCkS hElP fAx My BiG qUiZ.
+jAcKdAwS lOvE mY bIg SpHiNx Of QuArTz.
+hOw RaZoRbAcK jUmPiNg FrOgS cAn LeVeL sIx PiQuEd GyMnAsTs.
+bIg JuLy EaRtHqUaKeS cOnFoUnD zAnY eXpErImEnTaL vOw.
+tHe WiZaRd QuIcKlY jInXeD tHe GnOmEs BeFoRe ThEy VaPoRiZeD.
+wE qUiCkLy SeIzEd ThE bLaCk AxLe AnD jUsT sAvEd It FrOm HaRm.
+a QuIvErInG tExAs ZoMbIe FoUgHt RePuBlIc LiNkEd JeWeLrY.
+tHe ExPlOrEr WaS fRoZeN iN hIs BiG kAyAk JuSt AfTeR mAkInG qUeEr DiScOvErIeS.
+fEw BlAcK tAxIs DrIvE uP mAjOr RoAdS oN qUiEt HaZy NiGhTs.
+sIxTy ZiPpErS wErE qUiCkLy PiCkEd FrOm ThE wOvEn JuTe BaG.
+tHe QuIcK bRoWn FoX jUmPs OvEr A lAzY pArK rAnGeR dOg.
+a MaD bOxEr ShOt A qUiCk, gLoVeD jAb To ThE jAw Of HiS dIzZy OpPoNeNt.
+hEaVy BoXeS pErFoRm QuIcK wAlTzEs AnD jIgS.
+fIvE oR sIx BiG jEt PlAnEs ZoOmEd QuIcKlY bY tHe ToWeR.
+a LaRgE fAwN jUmPeD qUiCkLy OvEr WhItE zInC bOxEs.<br>
+<br>
+200 unique most used three letter combinations in English<br>
+the, and, ing, ion, tio, ent, ati, for, her, ter, hat, ere, ate, ith, his, con, res, ver, ons, one, men, ted, ive, oul, ove, int, est, com, not, per, can, but, are, als, its, out, any, our, man, ble, ect, day, car, war, here, een, use, son, cti, ear, ant, wit, inc, ore, ome, sta, tiv, pre, hav, ssi, eir, ide, har, han, age, tur, rth, tra, ery, por, ths, cts, lin, edi, unt, som, sed, lat, ver, ort, sin, tes, mer, sen, ind, ain, ele, ple, dis, exp, tat, pen, own, oth, uts, lan, ard, din, hel, til, ild, lly, dif, tte, ene, ven, par, nit, hor, ope, ely, rec, eve, emp, ect, ani, opt, nse, tiv, ena, yst, bel, ome, len, ona, cea, ual, sse, ult, nen, nal, ibe, ine, ric, ase, adi, yth, nin, ale, ber, ede, ove, elt, min, dge, ari, lin, ult, mme, sep, ade, off, cad, edi, ura, rge, hoo, nch, lic, ral, eth, eve, arb, oli, eld, urc, sst, eli, equ, cca, apr, nep, ecr, oal, ude, tol, udi, uct, odu, upo, oos, ide, ust, elp, off, ubj, rev, pul, ole, asi, rif, dul, ror, eph, rry, ryi, neu, elf, onc, now, inc, tle, ors, aca, ess, ile, dem, rot, tec, whi, uld, erg, niq, sce, ert, nan, nge, ocu, den, opt, nst, emp, uri, ade, ort, ais, ope, acy, ual, uch, uir, oks, ock, oad, nis, nif, ngs, vio, vel, rra, ele, rme, enr, epa, ntu, til, ntr, hro, rtn, rou, rge, ses, rds, ipa, ara, rro, tly, rma, ard, rai, ral, pho, rge, rdo, rde.<br>
+<br>
+200 unique most used three letter combinations in English UPPERCASE<br>
+THE, AND, ING, ION, TIO, ENT, ATI, FOR, HER, TER, HAT, ERE, ATE, ITH, HIS, CON, RES, VER, ONS, ONE, MEN, TED, IVE, OUL, OVE, INT, EST, COM, NOT, PER, CAN, BUT, ARE, ALS, ITS, OUT, ANY, OUR, MAN, BLE, ECT, DAY, CAR, WAR, HERE, EEN, USE, SON, CTI, EAR, ANT, WIT, INC, ORE, OME, STA, TIV, PRE, HAV, SSI, EIR, IDE, HAR, HAN, AGE, TUR, RTH, TRA, ERY, POR, THS, CTS, LIN, EDI, UNT, SOM, SED, LAT, VER, ORT, SIN, TES, MER, SEN, IND, AIN, ELE, PLE, DIS, EXP, TAT, PEN, OWN, OTH, UTS, LAN, ARD, DIN, HEL, TIL, ILD, LLY, DIF, TTE, ENE, VEN, PAR, NIT, HOR, OPE, ELY, REC, EVE, EMP, ECT, ANI, OPT, NSE, TIV, ENA, YST, BEL, OME, LEN, ONA, CEA, UAL, SSE, ULT, NEN, NAL, IBE, INE, RIC, ASE, ADI, YTH, NIN, ALE, BER, EDE, OVE, ELT, MIN, DGE, ARI, LIN, ULT, MME, SEP, ADE, OFF, CAD, EDI, URA, RGE, HOO, NCH, LIC, RAL, ETH, EVE, ARB, OLI, ELD, URC, SST, ELI, EQU, CCA, APR, NEP, ECR, OAL, UDE, TOL, UDI, UCT, ODU, UPO, OOS, IDE, UST, ELP, OFF, UBJ, REV, PUL, OLE, ASI, RIF, DUL, ROR, EPH, RRY, RYI, NEU, ELF, ONC, NOW, INC, TLE, ORS, ACA, ESS, ILE, DEM, ROT, TEC, WHI, ULD, ERG, NIQ, SCE, ERT, NAN, NGE, OCU, DEN, OPT, NST, EMP, URI, ADE, ORT, AIS, OPE, ACY, UAL, UCH, UIR, OKS, OCK, OAD, NIS, NIF, NGS, VIO, VEL, RRA, ELE, RME, ENR, EPA, NTU, TIL, NTR, HRO, RTN, ROU, RGE, SES, RDS, IPA, ARA, RRO, TLY, RMA, ARD, RAI, RAL, PHO, RGE, RDO, RDE.<br>
+<br>
+200 unique most used three letter combinations in English UPPERCASE<br>
+tHe, aNd, iNg, iOn, tIo, eNt, aTi, fOr, hEr, tEr, hAt, eRe, aTe, iTh, hIs, cOn, rEs, vEr, oNs, oNe, mEn, tEd, iVe, oUl, oVe, iNt, eSt, cOm, nOt, pEr, cAn, bUt, aRe, aLs, iTs, oUt, aNy, oUr, mAn, bLe, eCt, dAy, cAr, wAr, hErE, EeN, UsE, SoN, CtI, EaR, AnT, WiT, InC, OrE, OmE, StA, TiV, PrE, HaV, SsI, EiR, IdE, HaR, HaN, AgE, TuR, RtH, TrA, ErY, PoR, ThS, CtS, LiN, EdI, UnT, SoM, SeD, LaT, VeR, OrT, SiN, TeS, MeR, SeN, InD, AiN, ElE, PlE, DiS, ExP, TaT, PeN, OwN, OtH, UtS, LaN, ArD, DiN, HeL, TiL, IlD, LlY, DiF, TtE, EnE, VeN, PaR, NiT, HoR, OpE, ElY, ReC, EvE, EmP, EcT, AnI, OpT, NsE, TiV, EnA, YsT, BeL, OmE, LeN, OnA, CeA, UaL, SsE, UlT, NeN, NaL, IbE, InE, RiC, AsE, AdI, YtH, NiN, AlE, BeR, EdE, OvE, ElT, MiN, DgE, ArI, LiN, UlT, MmE, SeP, AdE, OfF, CaD, EdI, UrA, RgE, HoO, NcH, LiC, RaL, EtH, EvE, ArB, OlI, ElD, UrC, SsT, ElI, EqU, CcA, ApR, NeP, EcR, OaL, UdE, ToL, UdI, UcT, OdU, UpO, OoS, IdE, UsT, ElP, OfF, UbJ, ReV, PuL, OlE, AsI, RiF, DuL, RoR, EpH, RrY, RyI, NeU, ElF, OnC, NoW, InC, TlE, OrS, AcA, EsS, IlE, DeM, RoT, TeC, WhI, UlD, ErG, NiQ, ScE, ErT, NaN, NgE, OcU, DeN, OpT, NsT, EmP, UrI, AdE, OrT, AiS, OpE, AcY, UaL, UcH, UiR, OkS, OcK, OaD, NiS, NiF, NgS, ViO, VeL, RrA, ElE, RmE, EnR, EpA, NtU, TiL, NtR, HrO, RtN, RoU, RgE, SeS, RdS, IpA, ArA, RrO, TlY, RmA, ArD, RaI, RaL, PhO, RgE, RdO, RdE.<Br>
+<br>
+
+"""
